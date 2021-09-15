a538bf3774e8f5b0376b95210e367eb1969ae149
Martin Coronel <martincxx@gmail.com>
2021-09-15 13:27:06 +0300

    Update selectors and expected values for webdriver.io pages

--- test/specs/webdriverioWebSiteLinks.js
+++ test/specs/webdriverioWebSiteLinks.js
@@ -1,33 +1,34 @@
-describe('Verify whether user is able to navigate to ' +
-    'all links from WebdriverIO Home Page ', () => {
-
-    it('User should be able to launch WebdriverIO ' +
-        'Home Page', () => {
-        browser.url('https://webdriver.io/');
-        const title = browser.getTitle();
-        browser.pause(1000);
-        assert.equal(title, 'WebdriverIO · ' +
-            'Next-gen WebDriver test ' +
-            'framework for Node.js',
-            "title is not 'WebdriverIO · " +
-            "Next-gen WebDriver test framework " +
-            "for Node.js'");
+describe(
+  "Verify whether user is able to navigate to " +
+    "all links from WebdriverIO Home Page ",
+  () => {
+    it("User should be able to launch WebdriverIO " + "Home Page", () => {
+      browser.url("https://webdriver.io/");
+      const title = browser.getTitle();
+      browser.pause(1000);
+      assert.equal(
+        title,
+        "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO"
+      );
     });
 
-    it('User should be able to navigate to Get Started ' +
-        'Page of ' +
-        'WebdriverIO website', () => {
-        browser.url('https://webdriver.io/');
-        const element = $('//*[@href="/docs/gettingstarted.' +
-            'html"]');
+    it(
+      "User should be able to navigate to Get Started " +
+        "Page of " +
+        "WebdriverIO website",
+      () => {
+        browser.url("https://webdriver.io/");
+        const element = $(
+          "#__docusaurus > div.main-wrapper > header > div > div.buttons_1r9m > a:nth-child(1)"
+        );
         element.click();
         browser.pause(1000);
         const title = browser.getTitle();
-        expect(title).to.be.
-        equal('Getting Started · ' +
-            'WebdriverIO',
-            "title is not ''Getting Started · WebdriverIO'");
-
-    });
-
-});
+        expect(title).to.be.equal(
+          "Getting Started | WebdriverIO",
+          "title is not 'Getting Started | WebdriverIO'"
+        );
+      }
+    );
+  }
+);
